1f6e0b23924a960c80fbea7acab19839560efd18
Тигр <amzkryvchenko@gmail.com>
2022-01-20 23:49:32 -0400

    3 sections added

---
+++ app.js
@@ -0,0 +1,13 @@
+let mobileMenu = document.querySelector(".burger-menu");
+console.log(mobileMenu);
+let mainMenu = document.querySelector(".navigation");
+console.log(mainMenu);
+
+mobileMenu.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active-menu");
+    if(mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu")
+    } else {
+        mainMenu.classList.remove("active-menu")
+    }
+});
